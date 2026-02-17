8417aa82ab5e32b86824ddff6ba7acb6876b7878
Ahmet Canbaz <ahmet-canbaz@hotmail.com>
2026-02-18 00:43:34 +0300

    email fixed

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -3,13 +3,21 @@ import brevo from '@getbrevo/brevo';
 import { env } from '../utils/env.js';
 import { SMTP } from '../constants/SMPT.js';
 
-const apiInstance = new brevo.TransactionalEmailsApi();
-apiInstance.setApiKey(
-  brevo.TransactionalEmailsApiApiKeys.apiKey,
-  env(SMTP.BREVO_API_KEY),
-);
+let apiInstance = null;
+
+const getApiInstance = () => {
+  if (!apiInstance) {
+    apiInstance = new brevo.TransactionalEmailsApi();
+    apiInstance.setApiKey(
+      brevo.TransactionalEmailsApiApiKeys.apiKey,
+      env(SMTP.BREVO_API_KEY),
+    );
+  }
+  return apiInstance;
+};
 
 export const sendEmail = async (options) => {
+  const apiInstance = getApiInstance();
   const sendSmtpEmail = new brevo.SendSmtpEmail();
   sendSmtpEmail.subject = options.subject;
   sendSmtpEmail.htmlContent = options.html;
